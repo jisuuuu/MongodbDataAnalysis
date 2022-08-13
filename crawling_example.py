149f6f067537308734155fd8856de604a9c00930
gjkl55 <gjkl55@naver.com>
2022-08-13 20:19:19 +0900

    크롤링 예제 - 씨네 21 정보 정리

---
+++ crawling_example.py
@@ -0,0 +1,9 @@
+# 씨네 21 - 배우 정보 크롤링
+# 페이지를 이동해도 url 주소가 변하지 않는 상황
+# 개발자 도구 - network - Preserve log check - ALL
+# 씨네 21 - request 방식 => POST
+
+# section = 'actor'
+# period_start = '2022-02'
+# gender = 'all'
+# page = 3
