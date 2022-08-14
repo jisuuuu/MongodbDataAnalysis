bc93f3f931e0c210aab58caa29113f6034132400
gjkl55 <gjkl55@naver.com>
2022-08-14 20:19:19 +0900

    크롤링 예제 - 씨네 21 배우 이름 크롤링, 정규표현식으로 원하는대로 출력

--- crawling_example.py
+++ crawling_example.py
@@ -7,3 +7,31 @@
 # period_start = '2022-02'
 # gender = 'all'
 # page = 3
+
+# 1. 라이브러리 import
+from bs4 import BeautifulSoup
+import requests
+import pymongo
+import re
+
+# 2. mongodb connection
+conn = pymongo.MongoClient()
+actor_db = conn.cine21
+actor_collection = actor_db.actor_collection
+
+# 3. 크롤링 주소 requests
+cine21_url = 'http://www.cine21.com/rank/person/content'
+post_data = dict()
+post_data['section'] = 'actor'
+post_data['period_start'] = '2021-08'
+post_data['gender'] = 'all'
+post_data['page'] = 1
+
+res = requests.post(cine21_url, data=post_data)
+
+# 4. parsing과 데이터 추출
+soup = BeautifulSoup(res.content, 'html.parser')
+
+actors = soup.select('li.people_li div.name')
+for actor in actors:
+    print(re.sub('\(\w*\)', '', actor.text))
