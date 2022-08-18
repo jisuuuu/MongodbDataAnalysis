1f6ad500220bc380d243f6796f5eabfb28cc2ece
gjkl55 <gjkl55@naver.com>
2022-08-18 20:57:47 +0900

    크롤링 예제 - 씨네 21 몽고DB 저장을 위한 데이터 변환, 내용 합치기

--- crawling_example.py
+++ crawling_example.py
@@ -32,24 +32,38 @@ res = requests.post(cine21_url, data=post_data)
 # 4. parsing과 데이터 추출
 soup = BeautifulSoup(res.content, 'html.parser')
 
-actors = soup.select('li.people_li div.name')
-for actor in actors:
-    print(re.sub('\(\w*\)', '', actor.text))
+# for actor in actors:
+#     print(re.sub('\(\w*\)', '', actor.text))
 
 # 5. 배우 상세 정보 추출
+# 6. 배우 흥행 지수와 흥행 영화
+# 7. 수집한 데이터 기반 사전 만들기
 # 특수한 정규 표현식
 # Greedy(.*) 욕심 가득한 것 vs Non-Greedy(.*?) 욕심을 줄인 것
 
+actors = soup.select('li.people_li div.name')
+hits = soup.select('ul.num_info > li > strong')
+movies = soup.select('ul.mov_list')
 actors_info_list = list()
 
-for actor in actors:
+for index, actor in enumerate(actors):
+    actor_name = re.sub('\(\w*\)', '', actor.text)
+    actor_hits = int(hits[index].text.replace(',', ''))
+    movie_titles = movies[index].select('li a span')
+    movie_title_list = list()
+    for movie_title in movie_titles:
+        movie_title_list.append(movie_title.text)
+
+    actor_info_dict = dict()
+    actor_info_dict['배우이름'] = actor_name
+    actor_info_dict['흥행지수'] = actor_hits
+    actor_info_dict['출연영화'] = movie_title_list
+
     actor_link = 'http://www.cine21.com' + actor.select_one('a').attrs['href']
     response_actor = requests.get(actor_link)
     soup_actor = BeautifulSoup(response_actor.content, 'html.parser')
     default_info = soup_actor.select_one('ul.default_info')
     actor_details = default_info.select('li')
-
-    actor_info_dict = dict()
 
     for actor_item in actor_details:
         actor_item_field = actor_item.select_one('span.tit').text
@@ -58,9 +72,3 @@ for actor in actors:
         actor_info_dict[actor_item_field] = actor_item_value
     actors_info_list.append(actor_info_dict)
 print(actors_info_list)
-
-# 6. 배우 흥행 지수 뽑기
-hits = soup.select('ul.num_info > li > strong')
-for index, actor in enumerate(actors):
-    print(re.sub('\(\w*\)', '', actor.text))
-    print(int(hits[index].text.replace(',', '')))
