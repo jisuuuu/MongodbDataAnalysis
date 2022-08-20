b9274ecdec88e14519b48a65a009ecbf1e890eb5
gjkl55 <gjkl55@naver.com>
2022-08-21 00:03:56 +0900

    데이터 분석을 위한 몽고 DB 명령어 예제 2

--- command_example.py
+++ command_example.py
@@ -31,3 +31,23 @@ docs = actor_collection.find({'생년월일' : {'$exists' : False}}, {'배우이
 print(3)
 for doc in docs:
     print(doc)
+
+# 4. find의 다양한 문법 - 필드값 범위로 검색
+docs = actor_collection.find({'흥행지수': {'$lt': 10000}},
+                             {'배우이름': 1, '출연영화': 1, '_id': 0})
+print(4)
+for doc in docs:
+    print(doc)
+
+# 5. find의 다양한 문법 - or
+docs = actor_collection.find({'$or': [{'출연영화': '한산: 용의 출현'}, {'출연영화': '명량'}]},
+                             {'배우이름': 1, '출연영화': 1, '_id': 0}).sort('배우이름')
+print(5)
+for doc in docs:
+    print(doc)
+
+docs = actor_collection.find({'흥행지수': {'$gte': 10000},'$or': [{'출연영화': '한산: 용의 출현'}, {'출연영화': '명량'}]},
+                             {'배우이름': 1, '출연영화': 1, '_id': 0}).sort('배우이름')
+print(5)
+for doc in docs:
+    print(doc)
